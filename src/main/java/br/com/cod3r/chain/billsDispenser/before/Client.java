1f91851f2397123911485f44c3a861d139c953db
Mateus Pereira Jorge <mateus.p.jorge@gmail.com>
2023-08-24 11:54:12 -0300

    created first example of design pattern chain of responsibility

---
+++ src/main/java/br/com/cod3r/chain/billsDispenser/before/Client.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.chain.billsDispenser.before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println();
+        Dispenser atm = new Dispenser();
+        atm.withdraw(230);
+        System.out.println("-----------");
+        atm.withdraw(66);
+    }
+
+}
